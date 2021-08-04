08f0a1c81c068029bfa30f29b9b35219ca983e98
Crowdbotics <team@crowdbotics.com>
2021-08-04 16:35:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "broken_bar_29459Identifier",
+  url: "https://broken_bar_29459Identifier.botics.co",
+  api: "https://broken_bar_29459Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
